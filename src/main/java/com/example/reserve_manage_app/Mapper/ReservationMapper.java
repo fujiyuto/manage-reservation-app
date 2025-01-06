a0cc8f361173fe63f9a554bfed8a83f4d78c2632
fujiyuto <yuto.fujiwara.itfarm@gmail.com>
2025-01-06 22:35:04 +0900

    予約データのマッパークラス作成

---
+++ src/main/java/com/example/reserve_manage_app/Mapper/ReservationMapper.java
@@ -0,0 +1,73 @@
+package com.example.reserve_manage_app.Mapper;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.example.constants.enums.reservation.Status;
+import com.example.reserve_manage_app.Entities.ReservationEntity;
+
+@Mapper
+public interface ReservationMapper {
+    /**
+     * 店IDを使用し、予約データを取得する
+     * @param  Long restaurantId 店ID
+     * @return List<ReservationEntity> 予約データの一覧
+     */
+    @Select("SELECT * FROM reservations WHERE restaurant_id = #{restaurantId}")
+    List<ReservationEntity> getRestaurantReservationData(@Param("restaurantId") Long restaurantId);
+
+    /**
+     * ユーザーIDを使用し、予約データを取得する
+     * @param  Long reservationId 店ID
+     * @return List<ReservationEntity> 予約データの一覧
+     */
+    @Select("SELECT * FROM reservations WHERE user_id = #{userId}")
+    List<ReservationEntity> getUserReservationData(@Param("userId") Long userId);
+
+    /**
+     * 予約データを作成する
+     * @param userId
+     * @param restaurantId
+     * @param reserveDateTime
+     * @param numberOfPeople
+     * @param status
+     * @param notes
+     */
+    @Insert("INSERT INTO reservations(user_id, restaurant_id, reserve_date_time, num_of_people, status, notes) VALUES(#{userId}, #{restaurantId}, #{reserveDateTime}, #{numberOfPeople}, #{status}, #{notes})")
+    void createReservation(
+        @Param("user_id") Long userId,
+        @Param("restaurant_id") Long restaurantId,
+        @Param("reserve_date_time") LocalDateTime reserveDateTime,
+        @Param("num_of_people") int numberOfPeople,
+        @Param("status") Status status,
+        @Param("notes") String notes
+    );
+
+    /**
+     * 予約データを更新する
+     * @param reservationId
+     * @param reserveDateTime
+     * @param numberOfPeople
+     * @param notes
+     */
+    @Update("UPDATE reservations SET reserve_date_time = #{reserveDateTime}, num_of_people = #{numberOfPeople}, notes = #{notes}")
+    void updateReservation(
+        @Param("reservation_id") Long reservationId,
+        @Param("reserve_date_time") LocalDateTime reserveDateTime,
+        @Param("num_of_people") int numberOfPeople,
+        @Param("notes") String notes 
+    );
+
+    /**
+     * 予約データのステータスを更新する
+     * @param status
+     */
+    @Update("UPDATE reservations SET status = #{status}")
+    void updateReservationStatus(@Param("status") Status status);
+}
