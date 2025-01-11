056648207437024f9c1123f86624643fe71f35c5
fujiyuto <yuto.fujiwara.itfarm@gmail.com>
2025-01-11 18:12:39 +0900

    カスタム例外クラス作成

---
+++ src/main/java/com/example/reserve_manage_app/Exceptions/DataNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.reserve_manage_app.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class DataNotFoundException extends RuntimeException  {
+    DataNotFoundException(String message) {
+        super(message);
+    }
+}
