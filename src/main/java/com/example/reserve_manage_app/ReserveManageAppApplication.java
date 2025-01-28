79bc17be270640417ee15817bc9c82baf765d341
fujiyuto <yuto.fujiwara.itfarm@gmail.com>
2025-01-28 20:44:14 +0900

    マッパーの設定を修正

--- src/main/java/com/example/reserve_manage_app/ReserveManageAppApplication.java
+++ src/main/java/com/example/reserve_manage_app/ReserveManageAppApplication.java
@@ -1,9 +1,11 @@
 package com.example.reserve_manage_app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.reserve_manage_app.mapper")
 public class ReserveManageAppApplication {
 
 	public static void main(String[] args) {
